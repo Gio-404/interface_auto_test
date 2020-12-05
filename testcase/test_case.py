904107cab110647ee496e7de2e47dcb9b7cdbfaf
18202754169 <metasploit.exp@qq.com>
2020-12-06 00:58:06 +0800

    添加case示例

---
+++ testcase/test_case.py
@@ -0,0 +1,21 @@
+#!usr/bin/env python3
+# coding:utf-8
+# Create Date:2019/5/16
+
+from common import create_data,request_method
+import readconf
+import unittest
+
+
+class TestCases(unittest.TestCase):
+    def setUp(self):
+        pass
+    
+    def tearDown(self):
+        pass
+
+    def test_case1(self):
+        pass
+
+    def test_case2(self):
+        pass
